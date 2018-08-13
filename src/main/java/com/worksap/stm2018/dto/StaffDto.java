53746b1d0f6eaa2a70b6ce0da8a44549be5487fd
He_Junfeng <he_j@worksap.co.jp>
2018-08-13 13:50:03 +0800

    init

---
+++ src/main/java/com/worksap/stm2018/dto/StaffDto.java
@@ -0,0 +1,12 @@
+package com.worksap.stm2018.dto;
+
+import lombok.Data;
+
+@Data
+public class StaffDto {
+    String id;
+    String name;
+    String place;
+    String language;
+
+}
---
+++ src/main/java/com/worksap/stm2018/model/Person.java
@@ -0,0 +1,12 @@
+package com.worksap.stm2018.model;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private Integer personId;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+}
