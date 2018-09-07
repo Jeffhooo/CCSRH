9b4e82cd50fb2bb0c7223e5eb0e2367ac40c8105
Jeff <junfenghecn@163.com>
2018-09-07 23:57:18 +0800

    2018-09-07
    Signed-off-by: Jeff <junfenghecn@163.com>

--- src/main/java/jeff/schedule/ScheduleTask.java
+++ src/main/java/jeff/schedule/ScheduleTask.java
@@ -25,14 +25,11 @@ public class ScheduleTask {
         this.workHistoryDao = daoFactory.getWorkHistoryDao();
     }
 
-    @Scheduled(cron = "0 5 0 ? * SUN")//work at 00:05 of Sunday every week
+    @Scheduled(cron = "0 0 0 ? * SUN")//work at 00:00 of Sunday every week
     public void transferArrangementToWorkHistory() {
         Calendar calendar = Calendar.getInstance();
-        int min = calendar.getActualMinimum(Calendar.DAY_OF_WEEK); //get begin date of current week
-        int current = calendar.get(Calendar.DAY_OF_WEEK); //get the number the today of current week
-        calendar.add(Calendar.DAY_OF_WEEK, min-current-7); //get the begin date of last week
         Date beginDate = calendar.getTime();
-        calendar.add(Calendar.DAY_OF_WEEK, 7); //get the end date of last week
+        calendar.add(Calendar.DAY_OF_WEEK, 7);
         Date endDate = calendar.getTime();
         List<ArrangementVo> arrangements = arrangementDao.list(
                 new Timestamp(beginDate.getTime()),
